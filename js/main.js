8f1c84aa0456aeb836805349def33c3affd493fb
Ellyana Linden <ellyanalinden@gmail.com>
2018-08-18 09:31:18 -0600

    Delete token

--- js/main.js
+++ js/main.js
@@ -80,7 +80,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'pk.eyJ1IjoiZWxseWFuYWxpbmRlbiIsImEiOiJjams4dmh6ZzQwNTJxM3BzMDczNjU0NXdsIn0.Tgmjn4Xfak1kYbjalsCAVQ',
+    mapboxToken: '',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
